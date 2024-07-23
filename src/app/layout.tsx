0ddc6e69a7891691ecb7fa5b31aa143a2251c3a1
skerrepy <omarhanaficontact@gmail.com>
2024-07-23 23:04:21 +0100

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Omar Hanafi",
-  description: "Omar Hanafi is a passionate full-stack developer committed to creating intuitive web experiences with a focus on user-centric design and scalable solutions.",
+  description:
+    "Omar Hanafi is a passionate full-stack developer committed to creating intuitive web experiences with a focus on user-centric design and scalable solutions.",
 };
 
 export default function RootLayout({
@@ -18,19 +19,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-      <meta property="og:image" content="/placeholder.png" />
-      <meta property="og:image:width" content="1200" />
-      <meta name="og:image:type" content="image/png" />
+        <meta property="og:image" content="/placeholder.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta name="og:image:type" content="image/png" />
 
-      <meta property="og:image:height" content="630" />
-      <meta name="twitter:image" content="/placeholder.png" />
-      <meta name="twitter:image:type" content="image/png" />
-      <meta name="twitter:image:width" content="1200" />
-      <meta name="twitter:image:height" content="630" />
-
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:image" content="/placeholder.png" />
+        <meta name="twitter:image:type" content="image/png" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
       </Head>
 
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
